c7a67e81f46d750bc8b98d95e117003029014407
yohskua <guillaume.ligner@gmail.com>
2020-07-21 16:55:14 +0200

    refacto: add some typing to post_request.py module

--- post_request.py
+++ post_request.py
@@ -1,8 +1,11 @@
+from typing import Optional
 import requests
 import json
 
 
-def post_classify_ticket(message: str, url: str = "http://127.0.0.1", port=None):
+def post_classify_ticket(
+    message: str, url: str = "http://127.0.0.1", port: Optional[int] = None
+):
     post_url = f"{url}"
     post_url += f":{port}" if port is not None else ""
     post_url += "/ticket_support_classification"
